bca0f864fa8fe89864945404f3b3de6582c0b5cc
Vadim Bolshakov <vadimbolsh@gmail.com>
2023-08-09 06:03:23 +0400

    Add files via upload

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
-# Healthy_lifestyle
-Bot sale the Dietary Supplements
+Project online sale 
+
+TOKEN_BOT=6663365280:AAF8wuLotYDj6H3byhHdP7la-ZfHEeimwe4
---
+++ app.js
@@ -0,0 +1,94 @@
+let tg = window.Telegram.WebApp;
+
+tg.expand();
+
+tg.MainButton.textCoior =  "#FFFFFF";
+tg.MainButton.backgroundColor = "#0088CC";
+
+let item = "";
+
+let btn1 = document.getElementById("btn1");
+let btn2 = document.getElementById("btn2");
+let btn3 = document.getElementById("btn3");
+let btn4 = document.getElementById("btn4");
+let btn5 = document.getElementById("btn5");
+let btn6 = document.getElementById("btn6");
+
+btn1.onclick = function() {
+    if (tg.MainButton.isVisible) {
+        tg.hide();
+    }
+    else {
+        tg.MainButton.setText("You clicked button 1");
+        item = "1";
+        tg.show();
+    }
+}
+
+btn2.onclick = function() {
+    if (tg.MainButton.isVisible) {
+        tg.hide();
+    }
+    else {
+        tg.MainButton.setText("You clicked button 2");
+        item = "2";
+        tg.show();
+    }
+}
+
+btn3.onclick = function() {
+    if (tg.MainButton.isVisible) {
+        tg.hide();
+    }
+    else {
+        tg.MainButton.setText("You clicked button 3");
+        item = "3";
+        tg.show();
+    }
+}
+
+btn4.onclick = function() {
+    if (tg.MainButton.isVisible) {
+        tg.hide();
+    }
+    else {
+        tg.MainButton.setText("You clicked button 4");
+        item = "4";
+        tg.show();
+    }
+}
+
+btn5.onclick = function() {
+    if (tg.MainButton.isVisible) {
+        tg.hide();
+    }
+    else {
+        tg.MainButton.setText("You clicked button 5");
+        item = "5";
+        tg.show();
+    }
+}
+
+btn6.onclick = function() {
+    if (tg.MainButton.isVisible) {
+        tg.hide();
+    }
+    else {
+        tg.MainButton.setText("You clicked button 6");
+        item = "6";
+        tg.show();
+    }
+}
+
+Telegram.WebApp.onEvent("mainButtonClicked", function() {
+    tg.sendData(item);
+    tg.MainButton.setText("You clicked button " + item);
+});
+
+let usercard = document.getElementById("usercard");
+
+let p = document.createElement("p");
+
+p.innerText = `$(tg.initDataUnsafe.user.first_name) $(tg.initDataUnsafe.user.last_name)`;
+
+usercard.appendChild(p);
